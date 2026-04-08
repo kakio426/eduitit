1926f18c3570e6dc0d3ef9373db0b00c79ee8f71
유병주 <yubyeongju@yubyeongjuui-MacBookAir.local>
2026-04-08 21:03:00 +0900

    Revert "refactor(home): remove legacy authenticated home bridge assets"
    This reverts commit f64daca7c28c10d5a1f16039471892bc4e5c35ef.

--- core/static/core/js/home_authenticated_v6.js
+++ core/static/core/js/home_authenticated_v6.js
@@ -48,7 +48,9 @@
                     ? form.querySelector('textarea[name="text"]')
                     : document.querySelector(
                         '[data-home-v6-quickdrop-form="true"] textarea[name="text"], ' +
-                        '[data-home-v6-mobile-quickdrop-form="true"] textarea[name="text"]'
+                        '[data-home-v6-mobile-quickdrop-form="true"] textarea[name="text"], ' +
+                        '[data-home-v4-quickdrop-form="true"] textarea[name="text"], ' +
+                        '[data-home-v4-mobile-quickdrop-form="true"] textarea[name="text"]'
                     );
                 if (scopedInput && typeof scopedInput.focus === 'function') {
                     scopedInput.focus();
@@ -136,6 +138,8 @@
             },
         };
     };
+
+    window.homeV4Shell = window.homeV6Shell;
 
     function launchCard(card) {
         var href = card.dataset.launchHref || (card.dataset.productId ? '/products/' + card.dataset.productId + '/' : '');
